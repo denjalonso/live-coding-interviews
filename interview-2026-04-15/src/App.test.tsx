2fa1b9faef5a66fee191067021c9bcfd17cbe85f
Denis Juan Alonso Glez <denjalonso@gmail.com>
2026-04-15 11:47:58 +0100

    Add AuthCodeInput component and refactor App structure
    - Introduced a new `AuthCodeInput` component for handling OTP input functionality.
    - Updated `App` component to include `AuthCodeInput`.
    - Adjusted test structure in `App.test.tsx` (e.g., changed `it` to `test`).
    - Removed unused assets (`react.svg`, `vite.svg`, `hero.png`).

--- interview-2026-04-15/src/App.test.tsx
+++ interview-2026-04-15/src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import App from './App';
 
 describe('App', () => {
-  it('renders a top-level heading', () => {
+  test('renders a top-level heading', () => {
     render(<App />);
     expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
   });
--- interview-2026-04-15/src/App.tsx
+++ interview-2026-04-15/src/App.tsx
@@ -1,11 +1,64 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+type AuthCodeInput = {
+  onSumbit: () => void;
+};
+
+function AuthCodeInput({ onSumbit }: AuthCodeInput) {
+  const [inputs, setInputs] = useState<string[]>(Array(6).fill(''));
+  // Potentially move this to a reducer ...
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function handleReset() {
+    // TBD
+  }
+  function handleSubmit() {
+    onSumbit();
+  }
+
+  if (loading) {
+    return <>Loading...</>;
+  }
+
+  if (errorMessage && !loading) {
+    return <>Something went wrong `${errorMessage}`</>;
+  }
 
   return (
-    <>
+    <form onSubmit={handleSubmit}>
+      {inputs.map((v, i) => {
+        return (
+          <input
+            type="text"
+            key={i}
+            maxLength={1}
+            // the on change filters only digits in [0-9]
+            onChange={(e) => {
+              const newInputsArray = [...inputs];
+              newInputsArray[i] = e.target.value;
+              setInputs(newInputsArray);
+              e.target.focus()
+            }}
+          />
+        );
+      })}
+      <input type="submit" className="primary" />
+      <button onClick={handleReset} className="secondary">
+        Reset
+      </button>
+      {/*// Potentially we could move this to a component*/}
+      {success && <span>The OTP have succesfully send</span>}
+    </form>
+  );
+}
 
+function App() {
+  return (
+    <>
+      <AuthCodeInput />
     </>
   );
 }
